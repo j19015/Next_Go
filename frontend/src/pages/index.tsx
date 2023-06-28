ee43579579f01b2f12ce1073c8a2e21327d93b9f
j19015 <j19015@sangi.jp>
2023-06-29 02:47:57 +0900

    add useSttate && define title body

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+//nextでページ遷移を行うため
 import Link from 'next/link';
+//apiにあるcreateBookメソッドをimport
+import {createBook} from '../lib/api'
+//useStateを用いて状態管理
+import { useState } from 'react';
 
 const Home = () => {
+  //title,bodyを定義
+  const [title,setTitle]=useState('');
+  const [body,setBody]=useState('');
+  
   return (
     <div>
       <h1>Book投稿サイト</h1>
